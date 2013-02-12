e56d9164ff5367a8f9c002943d298e1a1bd9d65e
geojeff <geojeff@me.com>
2013-02-12 15:06:59 -0600

    Made the test for news listing at least set the default view and check for number of items. Interactive testing shows the news_listing default view to be available along with default and folder_view, but in testing attempts (commented out), there is a duplication of news_listing in default views.

--- kotti_newsitem/tests/test_views.py
+++ kotti_newsitem/tests/test_views.py
@@ -1,15 +1,28 @@
 from datetime import date
 from random import randint
 
+from pyramid.httpexceptions import HTTPFound
+
 from kotti.resources import get_root
 from kotti.resources import Document
-from kotti.views.view import view as kotti_view
-
+from kotti.resources import TypeInfo
+from kotti.testing import DummyRequest
 from kotti.views.edit.default_views import DefaultViewSelection
+from kotti.views.util import render_view
 
 from kotti_newsitem.resources import NewsItem
 from kotti_newsitem.views import NewsItemListViews
 from kotti_newsitem.views import NewsItemView
+
+
+class NewsItemDummyRequest(DummyRequest):
+
+    def __init__(self, context=None):
+        super(NewsItemDummyRequest, self).__init__()
+        self.context = context
+
+    def static_url(self, name):
+        return ''  # pragma: no cover
 
 
 def test_newsitem_view(db_session, dummy_request):
@@ -66,4 +79,29 @@ def test_news_item_list_views(db_session, dummy_request):
 
     dummy_request.registry.settings['kotti_newsitem.num_news'] = 4
 
+    assert len(view.news_listing()['items']) == 4
+
+    root['news'] = Document(title=u'News')
+
+    # What default views should be, and indeed are that in interactive
+    # testing:
+    #
+    # default_views = [('default', u'Default view'),
+    #                  ('folder_view', u'Folder view'),
+    #                  ('news_listing', u'News Listing'), ]
+    #
+    # The assert below fails because it shows as a list with duplicated News
+    # Listing items (second item should not be News Listing):
+    #
+    #    [('default', u'Default view'),
+    #     ('news_listing', u'News Listing'),
+    #     ('news_listing', u'News Listing'), ]
+    #
+    # assert root['news'].type_info.selectable_default_views == default_views
+
+    request = DummyRequest(GET={'view_name': 'news_listing'})
+    default_view = DefaultViewSelection(root["news"], request)
+    assert type(default_view.set_default_view()) == HTTPFound
+
+    view = NewsItemListViews(root['news'], dummy_request)
     assert len(view.news_listing()['items']) == 4
